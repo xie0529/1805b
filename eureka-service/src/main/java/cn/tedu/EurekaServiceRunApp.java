a423d98efaef1177458d2bed6e15b4b96ecd5656
xie0529 <15672487875@163.com>
2018-10-16 12:20:50 +0800

    java

---
+++ eureka-service/src/main/java/cn/tedu/EurekaServiceRunApp.java
@@ -0,0 +1,18 @@
+package cn.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceRunApp {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(EurekaServiceRunApp.class, args);
+		
+	}
+
+}
